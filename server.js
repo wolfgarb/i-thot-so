f58d8a1537b271ef8fdcd99a35f82560d386e2f5
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-11-12 14:57:09 -0500

    Notes successfully loading and saving

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { nanoid } = require('nanoid');
 
 const app = express();
 const PORT = process.env.PORT || 3005
@@ -11,14 +12,51 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// routes
-app.get("/", function(req, res) {
-    return res.sendFile(path.join(__dirname, "./public/index.html"));
+// GET routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get("/notes", function(req, res) {
-    return res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+// get notes by ID from sidebar
+app.get('/api/notes/:id', (req, res) => {
+    let pickedNote = req.params.id;
+    let noteData = JSON.parse(fs.readFileSync('./db/db.json'));
+    
+    for (let i = 0; i < noteData.length; i++) {
+        if (pickedNote === noteData[i].id) {
+            return res.json(array[i]);
+        } else {
+            res.send(404);
+        }
+    }
+});
+
+// POST route
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNotes = req.body;
+    newNotes.id = nanoid(5);
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+    // validate data
+});
+
+// DELETE notes
+
+
+// wildcard route takes user back to root directory
+app.get('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}`);
